1d224531ca3fb04cd586d77787875e6b823b0401
Nana-shi <leof.pereira2@gmail.com>
2022-08-31 17:43:27 -0300

    Atividades 009-Strings 31/08

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+/*Exercicio1*/
+
+const nome = prompt("Diga o seu nome:")
+let comida1 = prompt("Digite a sua comida favorita:")
+let comida2 = prompt("Digite a sua segunda comida favorita:")
+let comida3 = prompt("Digite a sua terceira comida favorita:")
+
+console.log(`As comidas favoritas do usuario: ${nome}, são respectivamente: \n${comida1}\n${comida2}\n${comida3}`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+const fraseTrocada = (frase.replaceAll("verde", "rosa"))
+const fraseTrocada2 = (fraseTrocada.replaceAll("azul", "laranja"))
+const fraseMaiuscula = "MAS NÃO DEIXE O GATO SAIR\""
+
+console.log(fraseTrocada2)
+
+console.log(fraseTrocada2.includes("verde"))
+console.log(fraseTrocada2.includes("laranja"))
+
+console.log(fraseTrocada2.replace("mas não deixe o gato sair\"" , fraseMaiuscula))
+
+console.log(frase.substr(0, 73) + frase.slice(74).toUpperCase())
